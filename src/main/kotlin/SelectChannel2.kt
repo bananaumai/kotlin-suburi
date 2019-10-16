1d38e072a648ddc54229ecce8be18408221fe7ab
YUTA SHIMAKAWA <banana.umai@gmail.com>
2019-10-16 14:37:23 +0900

    some

---
+++ src/main/kotlin/SelectChannel2.kt
@@ -0,0 +1,40 @@
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.onReceiveOrNull
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.selects.select
+
+@ExperimentalCoroutinesApi
+fun main() = runBlocking {
+    val c1 = produce {
+        repeat(4) { send(1); delay(100) }
+    }
+
+    val c2 = produce {
+        repeat(4) { send(2); delay(200) }
+    }
+
+    launch {
+        while (true) {
+            select<Unit> {
+                c1.onReceiveOrNull {
+                    if (it != null) {
+                        println(it)
+                    }
+                }
+                c2.onReceiveOrNull {
+                    if (it != null) {
+                        println(it)
+                    }
+                }
+            }
+            if (c1.isClosedForReceive && c2.isClosedForReceive) {
+                break
+            }
+        }
+    }.join()
+
+    println("finish")
+}
